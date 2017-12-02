2a44a8ae6208dcb3b2947a7e02583c790ff88739
Unknown <raphael.belanrenaud@gmail.com>
2017-12-02 10:52:45 -0500

    Scene work
    Enemy destinatiins added

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -3,26 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour {
+
+	public enum EnemyStates {Waiting, Moving}
 
 	public float destinationMinX = -5f;
 	public float destinationMaxX = 5f;
 	public float destinationMinZ = -5f;
 	public float destinationMaxZ = 5f;
 
+	public List<Transform> targets;
+
 	public float waitingDelayMin = 3f;
 	public float waitingDelayMax = 3f;
 
-
+	public EnemyStates state = EnemyStates.Moving;
 
 	private NavMeshAgent agent;
+	private EnemyManager manager;
 
 	void Start () {
-		
+		agent = GetComponent<NavMeshAgent> ();
+		Move ();
 	}
 
 	void Update () {
-		
+		switch (state) {
+		case EnemyStates.Moving:
+			if (agent.remainingDistance <= agent.stoppingDistance) {
+				StartCoroutine (Wait ());
+				state = EnemyStates.Waiting;
+			}
+			break;
+		case EnemyStates.Waiting:
+			break;
+		default:
+			break;
+		}
 	}
 
 	IEnumerator Wait () {
@@ -32,5 +50,6 @@ public class Enemy : MonoBehaviour {
 
 	void Move () {
 		agent.SetDestination (new Vector3 (Random.Range (destinationMinX, destinationMaxX), transform.position.y, Random.Range (destinationMinZ, destinationMaxZ)));
+		state = EnemyStates.Moving;
 	}
 }
